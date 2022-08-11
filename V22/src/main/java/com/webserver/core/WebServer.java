eb94eb1338be578f85dfbc3254ecd8e771bde143
zhoulei <675209316@qq.com>
2022-08-11 22:56:36 +0800

    Initial commit
    Signed-off-by: lei <675209316@qq.com>
    Signed-off-by: zhoulei <675209316@qq.com>

--- V22/src/main/java/com/webserver/core/WebServer.java
+++ V22/src/main/java/com/webserver/core/WebServer.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 /**
  * WebServer主类
@@ -11,11 +13,14 @@ import java.net.Socket;
  */
 public class WebServer {
     private ServerSocket serverSocket;
+    private ExecutorService threadPool;//创建一个线程池
 
     public WebServer() {
         try {
             System.out.println("正在启动服务端");
+
             serverSocket = new ServerSocket(8088);
+            threadPool = Executors.newFixedThreadPool(50);
             System.out.println("服务端启动完毕！");
         } catch (IOException e) {
             e.printStackTrace();
@@ -30,8 +35,7 @@ public class WebServer {
                 System.out.println("一个客户端连接了");
 //            启动一个线程来处理一个客服端的交互
                 ClientHandler handler = new ClientHandler(socket);
-                Thread t = new Thread(handler);
-                t.start();
+              threadPool.execute(handler);
             }
         } catch (IOException e) {
             e.printStackTrace();
